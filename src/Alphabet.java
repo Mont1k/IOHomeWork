9f04eefa4a2c327cd8aaa1a9287de67b4f4b7a54
Mont1k <nurtilekorozov@gmail.com>
2022-09-02 22:43:32 +0600

    adasdas

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Alphabet {
+public class Alphabet {
+    private String[] letters;
+    private int[]numbers;
+
+    public Alphabet(String[] letters, int[] numbers) {
+        this.letters = letters;
+        this.numbers = numbers;
+    }
+
+    public String[] getLetters() {
+        return letters;
+    }
+
+    public void setLetters(String[] letters) {
+        this.letters = letters;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (FileWriter fileWriter = new FileWriter("alphabet.txt")) {
+            for (char i = 'A'; i <= 'Z'; i++) {
+                String a=i + "" + Character.toLowerCase(i)+" \n";
+                fileWriter.append(a);
+            }
+
+            for (char i = '0'; i <='9' ; i++) {
+                fileWriter.append(i+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileReader fileReader=new FileReader("alphabet.txt")){
+            Scanner sc=new Scanner(fileReader);
+            int c=1;
+            while (sc.hasNextLine()){
+                System.out.println(c+") "+sc.nextLine());
+                c++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
